6cbcb1e7ae7211a46c882c18e635b027e256d9d2
Jeff Spooner <jdspoon@telus.net>
2015-03-23 09:28:30 -0600

    Updated geometry shader

--- src/geometryShader.glsl
+++ src/geometryShader.glsl
@@ -1,7 +1,25 @@
-in vec4 outValue;
-out vec4 finalColor;
+#version 410 core
 
-void main()
-{
-  finalColor = vec4(0.0, 0.0, 0.0, 1.0);
-}
+layout(points) in;
+
+uniform vec3 cameraPosition;
+
+layout(triangle_strip, max_vertices = 4) out;
+
+out vec4 nextPosition;
+
+void main() {
+  // Emit a square centered at the given point
+  gl_Position = gl_in[0].gl_Position + vec4(-0.1, -0.1, 0.0, 0.0);
+  EmitVertex();
+
+  gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.1, 0.0, 0.0);
+  EmitVertex();
+
+  gl_Position = gl_in[0].gl_Position + vec4(0.1, -0.1, 0.0, 0.0);
+  EmitVertex();
+
+  gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.1, 0.0, 0.0);
+  EmitVertex();
+
+  EndPrimitive();
