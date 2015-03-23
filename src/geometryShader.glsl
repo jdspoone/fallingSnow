ce1081cccc7645b670c4f0f936393ae6faebd6ff
Jeff Spooner <jdspoon@telus.net>
2015-03-23 10:06:55 -0600

    Bug fix: geometry shader was broken

--- src/geometryShader.glsl
+++ src/geometryShader.glsl
@@ -6,20 +6,19 @@ uniform vec3 cameraPosition;
 
 layout(triangle_strip, max_vertices = 4) out;
 
-out vec4 nextPosition;
-
 void main() {
   // Emit a square centered at the given point
-  gl_Position = gl_in[0].gl_Position + vec4(-0.1, -0.1, 0.0, 0.0);
+  gl_Position = gl_in[0].gl_Position + vec4(-0.05, -0.05, 0.0, 0.0);
   EmitVertex();
 
-  gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.1, 0.0, 0.0);
+  gl_Position = gl_in[0].gl_Position + vec4(-0.05, 0.05, 0.0, 0.0);
   EmitVertex();
 
-  gl_Position = gl_in[0].gl_Position + vec4(0.1, -0.1, 0.0, 0.0);
+  gl_Position = gl_in[0].gl_Position + vec4(0.05, -0.05, 0.0, 0.0);
   EmitVertex();
 
-  gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.1, 0.0, 0.0);
+  gl_Position = gl_in[0].gl_Position + vec4(0.05, 0.05, 0.0, 0.0);
   EmitVertex();
 
   EndPrimitive();
+}
