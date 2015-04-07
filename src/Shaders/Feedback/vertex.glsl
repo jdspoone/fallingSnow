b7b39f29f50cf7ee812de06e5bbd96b3d608a95b
Jeff Spooner <jdspoon@telus.net>
2015-04-06 20:34:50 -0600

    Removed unnecessary setting of gl_position in transform feedback vertex shader

--- src/Shaders/Feedback/vertex.glsl
+++ src/Shaders/Feedback/vertex.glsl
@@ -7,7 +7,6 @@ uniform mat4 MVP;
 out vec4 outVec;
 
 void main() {
-    gl_Position = MVP * vec4(inVec.xyz, 1.0);
     outVec = vec4(inVec.x, inVec.y - inVec.w, inVec.z, inVec.w);
     if (outVec.y < -1) {
       outVec.y = 1.00;
