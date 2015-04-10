ebb26e167dc226227266327073c6ab0d118a46a8
Jeff Spooner <jdspoon@telus.net>
2015-04-09 18:07:25 -0600

    Slowed down particle movement for testing purposes

--- src/Shaders/Feedback/vertex.glsl
+++ src/Shaders/Feedback/vertex.glsl
@@ -6,7 +6,7 @@ out vec3 nextPosition;
 
 void main() {
 
-  nextPosition = vec3(previousPosition.x, previousPosition.y - 0.0005, previousPosition.z);
+  nextPosition = vec3(previousPosition.x, previousPosition.y - 0.0001, previousPosition.z);
 
   if (nextPosition.y < -1) {
     nextPosition.y = 1.00;
