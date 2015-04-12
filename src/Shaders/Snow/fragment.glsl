1f1fd6efefa9cee3ecfd3c4b83dbf4bf9a740778
parkertf <parkertfish@gmail.com>
2015-04-12 14:06:38 -0600

    Added transparency back in, snowflakes are visible enough at this size

--- src/Shaders/Snow/fragment.glsl
+++ src/Shaders/Snow/fragment.glsl
@@ -8,8 +8,8 @@ out vec4 finalColor;
 void main()
 {
 //    // Gaussian Transparency
-//    float distance = pow(length(middle - position), 0.9);
+    float distance = pow(length(middle - position), 0.9);
 
     //White mode
-    finalColor = vec4(1.0f, 1.0f, 1.0f, 1.0);
+    finalColor = vec4(1.0f, 1.0f, 1.0f, max(0.5f * (0.002f - distance) / 0.001f, 0.00f));
 }
